6421cb501c9c06fdd2276cb8449b7b19d7a83fe3
GOVIND AVC <eemail2govindavc@gmail.com>
2025-02-24 15:51:54 +0530

    added changes

---
+++ client-testimonials/src/TestimonialCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const TestimonialCard = ({name,review}) => {
+    return(
+        <div>
+            <h1>{name}</h1>
+            <h3>{review}</h3>
+        </div>
+    )
+}
